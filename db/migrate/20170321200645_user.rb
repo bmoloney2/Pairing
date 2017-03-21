cdef4c8207c327c46237b2263dee1d587e90ffe2
Brian <bmoloney82@gmail.com>
2017-03-21 15:58:14 -0500

    userdb migration complete

---
+++ db/migrate/20170321200645_user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users, id: :uuid, default: "gen_random_uuid()", force: true do |t|
+      t.string :first_name, :length=>20, null: false
+      t.string :last_name, :length=>20, null: false
+      t.string :email, :length=>50, null: false
+      t.boolean :admin, null: false
+      t.boolean :profile_approved
+      t.text :password_digest, null: false
+      t.index([:email, :first_name], unique: true)
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170321200645) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "pgcrypto"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string  "first_name",       null: false
+    t.string  "last_name",        null: false
+    t.string  "email",            null: false
+    t.boolean "admin",            null: false
+    t.boolean "profile_approved"
+    t.text    "password_digest",  null: false
+    t.index ["email", "first_name"], name: "index_users_on_email_and_first_name", unique: true, using: :btree
+  end
+
+end
