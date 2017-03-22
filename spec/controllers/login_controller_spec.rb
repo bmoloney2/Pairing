9d0f85d039c7ea6227192b25e6b97a3288868fb5
Raul <rvasqz86@gmail.com>
2017-03-21 21:29:32 -0500

    created failing test

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
-
+  it "responds successfully with an HTTP 200 status code" do
+        get :login
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+  end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
-
+  it "responds successfully with an HTTP 200 status code" do
+        get :user
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+  end
 end
