fb68c1d5d36461ec7876dc29074d3ae595b56127
Raul <rvasqz86@gmail.com>
2017-03-22 16:20:16 -0500

    added to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
  resources :post
 
  get 'home/logout'
+ post 'post/edit'
 
-
-  devise_for :users
-  root to: "home#login"
+ devise_for :users
+root to: "home#login"
 end
