07bbbc356027694ef4ff35bb8314185bbfd305a5
Raul <rvasqz86@gmail.com>
2017-03-25 17:40:04 -0500

    post saves

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new
-    byebug
-    if @post.save(post_params)
+    @post.sender_id = current_user.id
+    @post.content = post_params["content"]
+    @post.title = post_params["title"]
+    @post.admin_message = post_params["admin_message"]
+    @post.recipient_id = post_params["recipient_id"]
+    @post.rating = post_params["rating"].to_i
+    if @post.save!
       flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
     else
@@ -25,7 +30,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    # byebug
     @posts_title = Post.find(params[:id]).title
     @posts_content = Post.find(params[:id]).content
     #will display a single post
@@ -60,7 +64,20 @@ class PostsController < ApplicationController
   end
 end
 
-private
+private 
+def post_params
+     params.require(:post).permit(:title, :content)
+ end
+
+def destroy
+  if @post.destroy
+    flash[:notice] = "Successfully deleted post!"
+    redirect_to post_path
+  else
+    flash[:alert] = "Errors deleting post!"
+  end
+end
+
 
 def post_params
   params.require(:post).permit(:title, :admin_message, :content, :sender_id, :recipient_id, :rating)
