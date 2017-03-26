de746ac3aeb6e4c25971eae70b778105136301df
Raul <rvasqz86@gmail.com>
2017-03-26 00:51:46 -0500

    Cleaned up new post controller, removed duplicate controller

--- app/controllers/post_controller.rb
+++
@@ -1,9 +0,0 @@
-class PostController < ApplicationController
-  def show
-    render :show 
-  end
-
-  def edit
-    render :edit, locals: { user: params[:user] }
-  end
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,29 @@
 class PostsController < ApplicationController
-
   before_action :authenticate_user!
-  # before_action :find_post
 
   def index
     @received_posts = Post.received_posts(current_user)
     @sent_posts = Post.sent_posts(current_user)
+    if current_user.admin
+      @all_posts = Post.all
+    end
   end
 
   def new
     @post = Post.new
     @users = User.all_except(current_user)
-    @reciever_id = params[:sender_id]
   end
 
   def create
     @post = Post.new(post_params)
     @post.sender_id = current_user.id
-      if @post.save(post_params)
-        flash[:notice] = "Successfully created post!"
-        redirect_to @post
-      else
-        flash[:alert] = "Error creating new post!"
-        render 'new'
-      end
+    if @post.save
+      flash[:notice] = "Successfully created post!"
+      redirect_to '/'
+    else
+      flash[:alert] = "Error creating new post!"
+      render 'new'
+    end
   end
 
   def show
@@ -55,13 +55,7 @@ class PostsController < ApplicationController
   end
 
   private
-
   def post_params
      params.require(:post).permit(:recipient_id, :admin_message, :willing_to_work, :rating, :title, :content)
   end
-
-  # def find_post
-  #   @post = Post.find_by_id(params[:id])
-  # end
-
 end
